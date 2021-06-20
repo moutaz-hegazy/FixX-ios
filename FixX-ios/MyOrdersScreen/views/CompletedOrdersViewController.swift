61e8a07b3cca78a7a1d6944747489a6ef27f51dc
moutaz-hegazy <moutazhegazy93@gmail.com>
2021-06-20 16:44:42 +0200

    delegation fix

--- FixX-ios/MyOrdersScreen/views/CompletedOrdersViewController.swift
+++ FixX-ios/MyOrdersScreen/views/CompletedOrdersViewController.swift
@@ -17,3 +17,23 @@ class CompletedOrdersViewController: UIViewController {
     }
 
 }
+
+
+extension CompletedOrdersViewController : UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "completedOrdersCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
--- FixX-ios/MyOrdersScreen/views/OngoingOrdersViewController.swift
+++ FixX-ios/MyOrdersScreen/views/OngoingOrdersViewController.swift
@@ -22,3 +22,23 @@ class OngoingOrdersViewController: UIViewController {
     }
 
 }
+
+
+extension OngoingOrdersViewController : UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ongoingOrdersCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
